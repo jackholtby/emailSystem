c1cfcab4fa8bc8652a0ef5bba3b068f547fe3b20
Jack <jack.fw.holtby@gmail.com>
2019-02-04 15:54:06 +0800

    getEmails first draft

--- emaildb.py
+++ emaildb.py
@@ -5,6 +5,7 @@ DBNAME = "emaildb"
 def sendEmails():
     # Sends out emails according to the status of each user.
 
+
 def updateStatus():
     # Updates the state value for each user based on the following rules:
     # If user is 'Active' & last login more than 4 days ago: Mark "Not Responsive"
@@ -13,3 +14,10 @@ def updateStatus():
 
 def getEmails():
     # Returns all entries in the emaildb database
+    """Returns all emails from emaildb """
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select user_id, email_content, date_sent from emails order by time desc")
+    posts = c.fetchall()
+    db.close()
+    return posts
