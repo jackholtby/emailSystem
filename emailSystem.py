7e6f56123a75e2e6b02afecf3826e897fb754e9a
Jack <jack.fw.holtby@gmail.com>
2019-02-04 14:43:07 +0800

    Moved database accessing functions to emaildb.py

--- emailSystem.py
+++ emailSystem.py
@@ -1,10 +1,9 @@
 # A daily email system.
 
-
 from flask import Flask, request, redirect, url_for
 import schedule
 import time
-from emaildb import get_posts, add_post
+# from emaildb import updateStatus
 
 app = Flask(__name__)
 
@@ -15,13 +14,3 @@ dashboardStart = '''
 dashbaoardEnd = '''
 
 '''
-
-def sendEmails():
-    # Sends out emails according to the status of each userself.
-
-
-def updateUserState():
-    # Updates the state value for each user based on the following rules:
-    # If user is 'Active' & last login more than 4 days ago: Mark "Not Responsive"
-    # If user is 'Not responsive' & last login more than 2 days ago: Mark "Inactive"
-    # If user is 'Not responsive' & last login less than 2 days ago: Mark "Active"
--- emaildb.py
+++ emaildb.py
@@ -1,3 +1,12 @@
 import psycopg2
 
 DBNAME = "emaildb"
+
+def sendEmails():
+    # Sends out emails according to the status of each user.
+
+def updateStatus():
+    # Updates the state value for each user based on the following rules:
+    # If user is 'Active' & last login more than 4 days ago: Mark "Not Responsive"
+    # If user is 'Not responsive' & last login more than 2 days ago: Mark "Inactive"
+    # If user is 'Not responsive' & last login less than 2 days ago: Mark "Active"
