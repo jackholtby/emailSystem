75e4b9c39da06e6b379824c52e98cbb46c3aa1dc
Jack <jack.fw.holtby@gmail.com>
2019-02-04 15:58:56 +0800

    removed sendEmails from emaildb.py

--- emaildb.py
+++ emaildb.py
@@ -2,10 +2,6 @@ import psycopg2
 
 DBNAME = "emaildb"
 
-def sendEmails():
-    # Sends out emails according to the status of each user.
-
-
 def updateStatus():
     # Updates the state value for each user based on the following rules:
     # If user is 'Active' & last login more than 4 days ago: Mark "Not Responsive"
